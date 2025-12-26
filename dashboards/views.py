97ae67b1cddaaef77e74c7417135282a2c9f3ebe
ImSnakee <kaushikshaurya0@gmail.com>
2025-12-26 17:51:30 +0530

    dashboard blog crud functions

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from blogs.models import Category,Blog
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm
+from .forms import CategoryForm,BlogForm
+from django.template.defaultfilters import slugify
 
 # Create your views here.
 
@@ -49,3 +50,49 @@ def delete_category(request,pk):
     category = get_object_or_404(Category,pk=pk)
     category.delete()
     return redirect('categories')
+
+
+def blogs(request):
+    blogs = Blog.objects.all()
+    context ={
+        'blogs':blogs
+    }
+    return render(request,'dashboard/blogs.html',context)
+
+def add_blog(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST,request.FILES)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+            blog.slug = slugify(form.cleaned_data['title']) + '-' + str(blog.id)
+            blog.save()
+            return redirect('blogs')
+    form = BlogForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'dashboard/add_blog.html',context)
+
+
+def edit_blog(request,pk):
+    blog = get_object_or_404(Blog,pk=pk)
+    if request.method == 'POST':
+        form = BlogForm(request.POST,request.FILES,instance=blog)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.slug = slugify(form.cleaned_data['title']) + '-' + str(blog.id)
+            blog.save()
+            return redirect('blogs')
+    form = BlogForm(instance=blog)
+    context= {
+        'form' : form,
+        'blog' : blog,
+    }
+    return render(request, 'dashboard/edit_blog.html',context)
+
+def delete_blog(request,pk):
+    blog = get_object_or_404(Blog,pk=pk)
+    blog.delete()
+    return redirect('blogs')
