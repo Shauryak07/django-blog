bc11a778528e8c403ba6df8afc6356f2c39f426f
ImSnakee <kaushikshaurya0@gmail.com>
2025-12-26 16:48:37 +0530

    dashboard crud functions

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -2,7 +2,7 @@ from .models import Category
 from assignment.models import SocialLink
 
 def get_categories(request):
-    categories = Category.objects.all()
+    categories = Category.objects.all().order_by('-updated_at')
     return dict(categories=categories)
 
 
