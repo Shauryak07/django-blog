936232c6c0d26b812cc15ab42d8985145f4d9499
ImSnakee <kaushikshaurya0@gmail.com>
2025-12-26 20:33:33 +0530

    manage user functionality for managers

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from blogs.models import Category,Blog
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm,BlogForm
+from .forms import CategoryForm,BlogForm,UserForm,EditUserForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -52,6 +53,8 @@ def delete_category(request,pk):
     return redirect('categories')
 
 
+
+
 def blogs(request):
     blogs = Blog.objects.all()
     context ={
@@ -96,3 +99,48 @@ def delete_blog(request,pk):
     blog = get_object_or_404(Blog,pk=pk)
     blog.delete()
     return redirect('blogs')
+
+
+def users(request):
+    users = User.objects.all()
+    context ={
+        'users':users,
+    }
+    return render(request,'dashboard/users.html',context)
+
+
+def add_user(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = UserForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'dashboard/add_user.html',context)
+
+
+def edit_user(request,pk):
+    user = get_object_or_404(User,pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST,instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = EditUserForm(instance=user)
+    context= {
+        'form' : form,
+        'user' : user,
+    }
+    return render(request, 'dashboard/edit_user.html',context)
+
+def delete_user(request,pk):
+    user = get_object_or_404(User,pk=pk)
+    user.delete()
+    return redirect('users')
